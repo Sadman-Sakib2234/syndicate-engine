1ae0b5360d31c095370d3835110fb5b1da622d35
Sadman_Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-06-19 14:57:40 +0600

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import syndicate.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        // gets and runs window
+        Window window = Window.get();
+        window.run();
+    }
+}
